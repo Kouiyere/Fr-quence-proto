e2f8f9a3f1ff0d23c57650efc059422c5d0f8823
Kouiyere <arthur.de.bailliencourt@gmail.com>
2025-02-05 13:05:05 +0100

    in progress

--- Assets/Scripts/Wave Minigame/AnswerCurve.cs
+++ Assets/Scripts/Wave Minigame/AnswerCurve.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class AnswerCurve : MonoBehaviour
 {
     private ButtonScript[] buttonScript;
+    private Transform[] points;
+    private float[] finalYValues;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,11 @@ public class AnswerCurve : MonoBehaviour
         {
             buttonScript[i] = buttons[i].GetComponent<ButtonScript>();
         }
+
+        points = gameObject.GetComponentsInChildren<Transform>();
+        points = points.ToList().Skip(1).ToArray();
+
+        finalYValues = new float[points.Length];
     }
 
     // Update is called once per frame
