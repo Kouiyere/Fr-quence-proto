1a363c12e3b77a7033b172410d79d9a5dd82405e
Kouiyere <arthur.de.bailliencourt@gmail.com>
2025-02-05 12:55:47 +0100

    started answer line

--- Assets/Scripts/Wave Minigame/AnswerCurve.cs
+++ Assets/Scripts/Wave Minigame/AnswerCurve.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AnswerCurve : MonoBehaviour
 {
+    private ButtonScript[] buttonScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject[] buttons = GameObject.FindGameObjectsWithTag("Button");
+        buttons = buttons.Reverse().ToArray();
+        buttonScript = new ButtonScript[buttons.Length];
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttonScript[i] = buttons[i].GetComponent<ButtonScript>();
+        }
     }
 
     // Update is called once per frame
