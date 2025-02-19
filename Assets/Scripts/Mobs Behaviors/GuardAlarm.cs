de6ccaf3bf812521108145b992a31001b4faaa1b
Kouiyere <arthur.de.bailliencourt@gmail.com>
2025-02-19 12:56:38 +0100

    commit before merge from main

--- Assets/Scripts/Mobs Behaviors/GuardAlarm.cs
+++ Assets/Scripts/Mobs Behaviors/GuardAlarm.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardAlarm : MonoBehaviour
 {
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
