36929ac8e787f587f5a92a3fb3bc7dd330d82de5
NoahDevlop <noacaux@gmail.com>
2025-02-26 10:10:06 +0100

    POO test

--- Assets/ScriptsNoah/Hack/HackFireAlarm.cs
+++ Assets/ScriptsNoah/Hack/HackFireAlarm.cs
@@ -4,68 +4,60 @@ using UnityEngine;
 
 public class HackFireAlarm : HackObject
 {
-
     public GameObject fireAlarm;
     public HackComputer[] computers;
     public Material fireAlarmActivated;
     public Material fireAlarmDefault;
 
-    public float delay;
-    private float timeElapsed;
-    private bool alarmOn = false;
+    public bool alarmOn = false;
+    private MeshRenderer fireAlarmRenderer;
 
     private new void Start()
     {
         base.Start();
-        InvokeRepeating(nameof(AlarmVisual), 1f,1f);
+
+        if (fireAlarm != null)
+        {
+            fireAlarmRenderer = fireAlarm.GetComponent<MeshRenderer>();
+        }
+
+        InvokeRepeating(nameof(AlarmVisual), 1f, 1f);
     }
 
     private void Update()
     {
-        for(int i = 0; i < computers.Length; i++)
+        bool isFireDetected = false;
+        foreach (HackComputer computer in computers)
         {
-            if (computers[i].isOnFire)
+            if (computer.isOnFire && !isActivated)
             {
-                alarmOn = true;
-            }
-            else
-            {
-                alarmOn = false;
+                isFireDetected = true;
+                break;
             }
         }
+
+        alarmOn = isFireDetected;
     }
 
     protected new void ActivateOrNotObject()
     {
         base.ActivateOrNotObject();
-
-        if(isActivated)
-        {
-            alarmOn = true;
-        }
-
-        else
-        {
-            alarmOn = false;
-        }
+        alarmOn = isActivated;
     }
 
     private void AlarmVisual()
     {
+        if (fireAlarmRenderer == null) return;
+
         if (alarmOn)
         {
-            if (fireAlarm.GetComponent<MeshRenderer>().material == fireAlarmActivated)
-            {
-                fireAlarm.GetComponent<MeshRenderer>().material = fireAlarmDefault;
-            }
-            else if(fireAlarm.GetComponent<MeshRenderer>().material == fireAlarmDefault)
-            {
-                fireAlarm.GetComponent<MeshRenderer>().material = fireAlarmActivated;
-            }
+            fireAlarmRenderer.material = (fireAlarmRenderer.material == fireAlarmActivated)
+                ? fireAlarmDefault
+                : fireAlarmActivated;
         }
         else
         {
-            fireAlarm.GetComponent<MeshRenderer>().material = fireAlarmDefault;
+            fireAlarmRenderer.material = fireAlarmDefault;
         }
     }
 }
