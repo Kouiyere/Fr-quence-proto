52dd70a722f194ed16a86164591ba40f324a1555
Kouiyere <arthur.de.bailliencourt@gmail.com>
2025-03-06 15:45:43 +0100

    Switch to spherecast not fonctionnal

--- Assets/Scripts/Mobs Behaviors/HackDetection.cs
+++ Assets/Scripts/Mobs Behaviors/HackDetection.cs
@@ -7,42 +7,65 @@ public class HackDetection : MonoBehaviour
     public float fov = 45f;
     public float distance = 5f;
     private Color color = Color.white;
+    private GameObject hack;
 
     void Update()
     {
+        CanSeeHack();
         DebugFov(fov, distance, color);
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        HackObject hackScript = other.GetComponent<HackObject>();
-        if (hackScript != null && hackScript.isHacked)
-        {
-            CanSeeHack(other.gameObject);
-        }
-    }
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    HackObject hackScript = other.GetComponent<HackObject>();
+    //    if (hackScript != null && hackScript.isHacked)
+    //    {
+    //        CanSeeHack(other.gameObject);
+    //    }
+    //}
 
-    public bool CanSeeHack(GameObject hack)
+    public bool CanSeeHack()
     {
-        Vector3 toHack = new Vector3((hack.transform.position.x - transform.position.x), (hack.transform.position.y - transform.position.y), (hack.transform.position.z - transform.position.z));
-        float angle = Vector3.Angle(toHack, transform.forward);
-        if (angle <= fov)
+        int mask = LayerMask.GetMask("Hackable");
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, distance, transform.forward, out hit, mask))
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, hack.transform.position, out hit, Mathf.Infinity))
+            hack = hit.collider.gameObject;
+            HackObject hackScript = hack.GetComponent<HackObject>();
+            if (hackScript !=null && hackScript.isHacked)
             {
-                color = Color.red;
-                return true;
+                Vector3 toHack = new Vector3((hack.transform.position.x - transform.position.x), (hack.transform.position.y - transform.position.y), (hack.transform.position.z - transform.position.z));
+                float angle = Vector3.Angle(toHack, transform.forward);
+                if (angle <= fov)
+                {
+                    if (Physics.Raycast(transform.position, hack.transform.position, out hit, Mathf.Infinity))
+                    {
+                        color = Color.red;
+                        return true;
+                    }
+                    else
+                    {
+                        color = Color.yellow;
+                        return false;
+                    }
+                }
+                else
+                {
+                    color = Color.yellow;
+                    return false;
+                }
             }
             else
             {
-                color = Color.yellow;
+                hack = null;
+                color = Color.white;
                 return false;
             }
         }
         else
         {
-            color = Color.yellow;
+            hack = null;
+            color = Color.white;
             return false;
         }
     }
@@ -53,5 +76,10 @@ public class HackDetection : MonoBehaviour
         Vector3 extentRight = Vector3.Reflect(extentLeft, transform.right);
         Debug.DrawRay(transform.position, extentLeft * distance, color);
         Debug.DrawRay(transform.position, extentRight * distance, color);
+        if (CanSeeHack())
+        {
+            Vector3 toHack = new Vector3((hack.transform.position.x - transform.position.x), (hack.transform.position.y - transform.position.y), (hack.transform.position.z - transform.position.z));
+            Debug.DrawRay(transform.position, toHack, color);
+        }
     }
 }
