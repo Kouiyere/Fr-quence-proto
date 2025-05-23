2be52f937d43f00713693f7287ac2ea752bc0912
NoahDevlop <noacaux@gmail.com>
2025-05-23 16:23:39 +0200

    IA detectionHackOrFireIcon

---
+++ Assets/ScriptsNoah/IA/DetectionIcon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionIcon : MonoBehaviour
+{
+    public GameObject iconDetection;
+    private IADetection detection;
+
+    private void Start()
+    {
+        detection = GetComponent<IADetection>();
+    }
+
+    private void Update()
+    {
+        if(detection.CanSeeHack() != null || detection.SeeFire() != null)
+        {
+            iconDetection.SetActive(true);
+        }
+        else
+        {
+            iconDetection.SetActive(false);
+        }
+
+        if (Camera.main != null)
+        {
+            iconDetection.transform.LookAt(Camera.main.transform);
+            iconDetection.transform.rotation = Quaternion.LookRotation(iconDetection.transform.position - Camera.main.transform.position);
+        }
+    }
+
+}
