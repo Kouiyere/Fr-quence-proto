b6690236dfe509c25044d942967b4125beb3cf60
NoahDevlop <noacaux@gmail.com>
2025-05-24 23:52:54 +0200

    water/electricity propagation + source

--- Assets/ScriptsNoah/Electricity.cs
+++ Assets/ScriptsNoah/Electricity.cs
@@ -1,70 +1,101 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Electricity : MonoBehaviour
 {
     public GameObject particles;
-    public bool isElectrical = false;
+    public float electricityDuration = 2f;
+    public float neighborCheckRadius = 2f;
 
-    public float electricityDuration = 1f;
-    private float timer = 0f;
+    private float sourceTimer = 0f;
 
-    private Electricity electricitySource = null;
+    private static List<Electricity> allElectricObjects = new List<Electricity>();
+
+    public bool isSource = false;
+    private bool isElectrified = false;
 
     private void Start()
     {
         particles.SetActive(false);
+        allElectricObjects.Add(this);
+    }
+
+    private void OnDestroy()
+    {
+        allElectricObjects.Remove(this);
     }
 
     private void Update()
     {
-        if(electricitySource != null)
+        if (isSource)
         {
-            if(electricitySource.isElectrical)
+            sourceTimer += Time.deltaTime;
+            if (sourceTimer >= electricityDuration)
             {
-                SetElectrical();
+                isSource = false;
             }
         }
-        else
+
+        foreach (var obj in allElectricObjects)
         {
-            timer += Time.deltaTime;
-            if (timer >= electricityDuration)
+            obj.isElectrified = false;
+        }
+
+        foreach (var obj in allElectricObjects)
+        {
+            if (obj.isSource)
             {
-                ResetElectricity();
+                obj.PropagateElectricity(new HashSet<Electricity>());
             }
         }
+
+        particles.SetActive(isElectrified);
+    }
+
+    private void PropagateElectricity(HashSet<Electricity> visited)
+    {
+        if (visited.Contains(this)) return;
+        visited.Add(this);
+
+        isElectrified = true;
+
+        foreach (var neighbor in GetNearbyElectricObjects())
+        {
+            neighbor.PropagateElectricity(visited);
+        }
+    }
+
+    private List<Electricity> GetNearbyElectricObjects()
+    {
+        List<Electricity> neighbors = new List<Electricity>();
+        foreach (var other in allElectricObjects)
+        {
+            if (other == this) continue;
+            if ((transform.position - other.transform.position).sqrMagnitude <= neighborCheckRadius * neighborCheckRadius)
+            {
+                neighbors.Add(other);
+            }
+        }
+        return neighbors;
     }
 
     private void OnParticleCollision(GameObject particle)
     {
         if (particle.CompareTag("ElectricParticle") && particle != particles)
         {
-            SetElectrical();
+            BecomeElectricSource();
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void BecomeElectricSource()
     {
-        if (other.gameObject.CompareTag("Water"))
-        {
-            electricitySource = other.GetComponent<Electricity>();
-            print(electricitySource);
-
-        }
+        isSource = true;
+        sourceTimer = 0f;
     }
 
-    private void SetElectrical()
+    private void OnDrawGizmosSelected()
     {
-        isElectrical = true;
-        particles.SetActive(true);
-        timer = 0f;
-    }
-
-    private void ResetElectricity()
-    {
-        isElectrical = false;
-        particles.SetActive(false);
-        timer = 0f;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, neighborCheckRadius);
     }
 }
