b6c3578b1c0c8bde2e634fe6ae13d1c959041de7
NoahDevlop <noacaux@gmail.com>
2025-05-26 11:52:24 +0200

    Bigger Outline Test + wind propagation with collider script test

--- Assets/ScriptsNoah/WindPropagationCollider.cs
+++ Assets/ScriptsNoah/WindPropagationCollider.cs
@@ -2,33 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HackWind : MonoBehaviour
+public class WindPropagationCollider : MonoBehaviour
 {
-    private HackObject hackObject;
-    public GameObject palm;
-    public GameObject wind;
-    public bool windActivated = false;
-
-    private void Start()
-    {
-        hackObject = GetComponent<HackObject>();
-    }
-
-    void Update()
-    {
-        if (hackObject.isHacked)
-        {
-            windActivated = true;
-            wind.SetActive(true);
-            palm.transform.Rotate(new Vector3(0, 0, 15));
-        }
-        else
-        {
-            windActivated = false;
-            wind.SetActive(false);
-        }
-    }
-
+    /*
     private void OnTriggerEnter(Collider other)
     {
         if (!windActivated) return;
@@ -50,4 +26,5 @@ public class HackWind : MonoBehaviour
             fire.directionTarget = null;
         }
     }
+    */
 }
